ff29918b3b1006be73958a3ad7c47f517c942525
aguffroy <alban.guffroy@gmail.com>
2010-10-05 19:48:43 +0200

    ajout de boutons image

---
+++ src/maa/monPorteMonnaie/AccountDbAdapter.java
@@ -0,0 +1,5 @@
+package maa.monPorteMonnaie;
+
+public class AccountDbAdapter {
+
+}
--- src/maa/monPorteMonnaie/Accueil.java
+++ src/maa/monPorteMonnaie/Accueil.java
@@ -3,20 +3,30 @@ package maa.monPorteMonnaie;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class Accueil extends Activity implements View.OnClickListener {
-    private Button creditCardButton;
+    private ImageButton creditCardButton;
+    private ImageButton amexButton;
+    private EditText amount;
+    private TextView result;
 
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
-        creditCardButton = (Button) findViewById(R.id.myCreditCard);
+        amount = (EditText) findViewById(R.id.amount);
+        creditCardButton = (ImageButton) findViewById(R.id.myCreditCard);
+        amexButton = (ImageButton) findViewById(R.id.myAmex);
+        result = (TextView) findViewById(R.id.result);
         creditCardButton.setOnClickListener(this);
+        amexButton.setOnClickListener(this);
     }
 
     public void onClick(View v) {
+        result.setText(amount.getText());
     }
 }
